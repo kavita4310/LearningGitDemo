9669bdcb2fee2dd59c565ac21dbe7c61e30b3bae
kavita chauhan <kavita4310ios@gmail.com>
2026-07-23 07:27:10 +0530

    Initial Commit

---
+++ iOSGitGithubDemo/iOSGitGithubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSGitGithubDemoApp.swift
+//  iOSGitGithubDemo
+//
+//  Created by kavita chauhan on 23/07/26.
+//
+
+import SwiftUI
+
+@main
+struct iOSGitGithubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
